de4a38aa8973d862491c819da89ff4acfb5a7ed7
Tyler Hall <rth@tyler.io>
2020-08-22 22:14:59 -0500

    Initial Commit

---
+++ photoz/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  photoz
+//
+//  Created by Tyler Hall on 8/22/20.
+//  Copyright © 2020 Tyler Hall. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
